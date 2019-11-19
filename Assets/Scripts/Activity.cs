fb7cd34efc360a83d488b169eaf4de57cd399bd2
Erivan Souza da Silva Filho <erivan.filho@citsamazonas.local>
2019-11-19 13:41:19 -0400

    Project Base Created

---
+++ Assets/Scripts/Activity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activity : MonoBehaviour
+{
+    // Update is called once per frame
+    public void WrongAnswer()
+    {
+        
+    }
+
+    public void CorrectAnswer()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MainWord.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(AudioSource))]
+public class MainWord : MonoBehaviour
+{
+    AudioSource audioData;
+    int soundPlaying = 0;
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void GoToTask()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ManagerSound()
+    {
+        audioData = GetComponent<AudioSource>();    
+        
+        if (soundPlaying == 0) {
+            soundPlaying = 1;
+            audioData.Play();
+            Debug.Log("Started Audio");
+        } else {
+            soundPlaying = 0;
+            audioData.Pause();
+            Debug.Log("Started Audio");
+        }
+        
+    }
+
+    public void ExitGame()
+    {        
+        Application.Quit();
+    }
+}
